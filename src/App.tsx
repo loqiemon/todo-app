4f877e8977180a1c88d65f46df70e02912a551dd
Your Name <you@example.com>
2023-09-08 12:39:33 +0300

    [ADD]: Base routing and main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 
 import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage/MainPage'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
